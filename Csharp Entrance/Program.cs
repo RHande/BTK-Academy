44c7498b0e5979991c370140172a7004b104ef47
Hande <154692280+RHande@users.noreply.github.com>
2024-09-06 23:23:27 +0300

    Nullable types

--- Csharp Entrance/Program.cs
+++ Csharp Entrance/Program.cs
@@ -86,6 +86,50 @@ int myNum11 = (int)myDoubleNum2; // Manual casting: double to int
 Console.WriteLine(myNum11);//Shows 15
 
 
+//Nullable types
+
+int? myNum12 = null;
+double? myDoubleNum3 = 5.99D;
+char? myLetter1 = 'D';
+bool? myBool1 = true;
+string myText1 = null;
+
+Console.WriteLine(myNum12);
+Console.WriteLine(myDoubleNum3);
+Console.WriteLine(myLetter1);
+Console.WriteLine(myBool1);
+Console.WriteLine(myText1);
+
+//We can also use the ?? operator to assign a default value to a nullable type
+
+int? myNum13 = null;
+int myNum14 = myNum13 ?? 100;
+Console.WriteLine(myNum14); //Shows 100
+
+
+//We can also use the is operator to check the type of a variable
+
+int myNum15 = 15;
+Console.WriteLine(myNum15 is int); //Shows True
+
+
+//We can also use hasvalue property to check if a nullable type has a value
+
+int? maas = 10;
+
+if (maas.HasValue)//We can also use if (maas != null) instead of if (maas.HasValue)
+{//We can not use HasValue property with non-nullable types so we only use it with 'int?' type. '?' is used to make the type nullable
+    Console.WriteLine("Maas degeri: " + maas.Value);
+}
+else
+{
+    Console.WriteLine("Maas degeri: null");
+}
+
+int? maas1 = null; //It is same as int? maas1 = default;
+Console.WriteLine(maas1.HasValue);//Shows False
+Console.WriteLine(maas1.GetValueOrDefault());//Shows 0. Because the default value of int is 0
+
 
 
 
--- Csharp Entrance/obj/Debug/net8.0/Csharp Entrance.AssemblyInfo.cs
+++ Csharp Entrance/obj/Debug/net8.0/Csharp Entrance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp Entrance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22434f2cc0e3b4a0c0276b3aa00f0aec07c5980d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf344e8352ff9daeb0fd79eed85061ed41993047")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp Entrance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp Entrance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
